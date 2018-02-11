4db63f5f4c237e5c827fc6a37262cfae0f29f5d8
Felix <felixthe8@gmail.com>
2018-02-10 16:47:15 -0800

    User model mapping completed
    Patient, Medical Professional, and System Admin constructor attributes now map to inherited classes.

--- services/model/systemadmin.js
+++ services/model/systemadmin.js
@@ -2,6 +2,9 @@ var user = require('./user');
 var security = require('./security');
 
 function SystemAdmin (username, password) {
+    user.username = username;
+    user.password = password;
+
     this.username = user.username || '';
     this.password = user.password || '';
 }
