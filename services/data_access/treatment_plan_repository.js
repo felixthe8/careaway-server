73794ac6f6c38c9a25a1a8aca61954f31f3c9c63
Jimmy <iamjimmyjimmy@yahoo.com>
2018-02-19 23:08:49 -0800

    Added Comments
    Added comments to the treatment plan repository

--- services/data_access/treatment_plan_repository.js
+++ services/data_access/treatment_plan_repository.js
@@ -8,7 +8,13 @@ function TreatmentPlanAccess(dbConnection)
   //fetches the database client connection
   this.db = dbConnection;
 }
-
+/**
+ * This appends a new treatment plan to the patient 
+ * treatment array
+ * 
+ * @param {*} patient the username of the patient 
+ * @param {*} treatment the treatment plan that is going to be added
+ */
 TreatmentPlanAccess.prototype.CreateTreatment = function(patient,treatment){
   const collection = this.db.collection('Users');
   collection.updateOne(
@@ -18,20 +24,33 @@ TreatmentPlanAccess.prototype.CreateTreatment = function(patient,treatment){
       console.log('Added treatment');
    }
   );
-  console.log("Done");
 }
 
+/**
+ * This Edits an existing Treatment Plan
+ * 
+ * @param {*} patient the username of the patient
+ * @param {*} treatment the new edits to the treatment plan
+ */
 TreatmentPlanAccess.prototype.EditTreatment= function(patient,treatment){
   const collection = this.db.collection('Users');
   collection.updateOne(
-    {'username' : patient, 'accountType.treatment.label' : treatment.label, 'accountType.treatment.create_at': treatment.create_at},//looks for username in the database
+    {'username' : patient, //looks for the patient in the database
+     'accountType.treatment.label' : treatment.label, //looks for the type of treatment 
+     'accountType.treatment.create_at': treatment.create_at}, //looks for the date created of that treatment
     { $set: {'accountType.treatment.$': treatment}},//updates treatment with new edits on the array 
       function(err, result){
       console.log('Edited treatment');
     }
   );
 }
-
+/**
+ * This Deletes a Treatment Plan within the user's
+ * treatment array
+ * 
+ * @param {*} patient the username of the patient 
+ * @param {*} treatment the treatment plan that needs to be deleted
+ */
 TreatmentPlanAccess.prototype.DeleteTreatment= function(patient,treatment){
   const collection = this.db.collection('Users');
   collection.updateOne({'username' : patient},//looks for username in the database
@@ -40,7 +59,11 @@ TreatmentPlanAccess.prototype.DeleteTreatment= function(patient,treatment){
     console.log('Remove treatment');
   });
 }
-
+/**
+ * Gets all treatment plans of a particular patient
+ * 
+ * @param {*} patient the username of the patient
+ */
 TreatmentPlanAccess.prototype.GetPatientTreatment = function(patient){
   const collection = this.db.collection('Users');
   return new promise(function(fullfill,reject)
@@ -55,6 +78,7 @@ TreatmentPlanAccess.prototype.GetPatientTreatment = function(patient){
       else
       {
         console.log('Successfully got query');
+        //Error check to see if the result is empty
         if(result!=null){
           var treatments = result.accountType.treatment;
           //return an object containing all the treatments of the user
@@ -62,18 +86,26 @@ TreatmentPlanAccess.prototype.GetPatientTreatment = function(patient){
           fullfill(treatmentList);
         }
         else{
+          //return null if result is empty
           fullfill(null);
         }
       } 
     });
   });
 }
-
+/**
+ * Gets Treatments plans of a particular diagnosis
+ * under a particular medical professional
+ * 
+ * @param {*} MPCode the mediacl professional code
+ * @param {*} Diagnosis the designated diagnosis
+ */
 TreatmentPlanAccess.prototype.GetTreatmentsDiagnosis = function(MPCode,Diagnosis){
   const collection = this.db.collection('Users');
   return new promise(function(fullfill,reject)
   { 
-    collection.find({'accountType.role': 'patient', 'accountType.medicalcode': MPCode, 'accountType.diagnosis': Diagnosis}).toArray(function(err, docs) 
+    collection.find({'accountType.role': 'patient', 'accountType.medicalcode': MPCode, 'accountType.diagnosis': Diagnosis}).toArray(
+    function(err, docs) 
     {
       if(err)
       {
@@ -83,11 +115,13 @@ TreatmentPlanAccess.prototype.GetTreatmentsDiagnosis = function(MPCode,Diagnosis
       else
       {
         console.log('Successfully got query');
+        //puts all treatment plans into one array
         var Treatments =[];
         for(var i=0; i<docs.length; i++)
         {
           Treatments.push(docs[i].accountType.treatment)
         }
+        //returns the object of an array of treatment plan
         var results = {"Treatments" : Treatments}
         fullfill(results);
       }
