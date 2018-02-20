f0a54ac72b7e64f5e38342f7e3b87c652fdec55d
Jimmy <iamjimmyjimmy@yahoo.com>
2018-02-19 18:20:49 -0800

    Added Appointment and Treatment PlanRepository
    Declared the Appointment and Treatment Plan Data Access
    TODO: Define the functions

---
+++ services/data_access/treatment_plan_repository.js
@@ -0,0 +1,33 @@
+const mongoClient = require('mongodb').MongoClient;
+const promise = require('promise');
+/**
+ * Constructor of the User Repository
+ */
+function TreatmentPlanAccess(dbConnection)
+{
+  //fetches the database client connection
+  this.db = dbConnection;
+}
+
+TreatmentPlanAccess.prototype.CreateTreatment = function(){
+
+}
+
+TreatmentPlanAccess.prototype.EditTreatment= function(){
+
+}
+
+TreatmentPlanAccess.prototype.DeleteTreatment= function(){
+
+}
+
+TreatmentPlanAccess.prototype.GetPatientTreatment= function(){
+
+}
+
+TreatmentPlanAccess.prototype.GetTreatmentsDiagnosis = function(){
+    
+
+}
+
+module.exports = TreatmentPlanAccess
--- services/data_access/user_repository.js
+++ services/data_access/user_repository.js
@@ -29,6 +29,19 @@ UserAccess.prototype.Create = function(user)
   });
 };
 /**
+ * This function edits the patient diagnosis
+ * @param {*} username 
+ * @param {*} diagnosis
+ */
+UserAccess.prototype.EditPatientDiagnosis= function(username,diagnosis){
+  const collection = this.db.collection('Users');
+  collection.updateOne({'username' : username},//looks for username in the database
+                          { $set: {'accountType.diagnosis ': diagnosis}},//inserts new password 
+                           function(err, result){
+                            console.log('Updated Diagnosis');
+                        });
+}
+/**
  * This function edits a user's password into the 
  * mongodb database with received information
  * 
