815c19c19ba31a6a306b8469fa7bc1c0740a122c
lmao <fireclimbers@gmail.com>
2018-02-20 23:29:13 -0800

    cors

--- services/account_management/config/app.js
+++ services/account_management/config/app.js
@@ -18,6 +18,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(helmet());
 
+app.use(function(req,res,next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+})
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
