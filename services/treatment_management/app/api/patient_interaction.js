15ea06d3208582d448748f2707fa75b8debbe34d
lmao <fireclimbers@gmail.com>
2018-03-10 00:28:23 -0800

    created endpoints for updating, creating, and querying patient treatments

--- services/treatment_management/app/api/patient_interaction.js
+++ services/treatment_management/app/api/patient_interaction.js
@@ -1,86 +1,69 @@
 const api = {};
 
-api.patientUpdateMeter = (models) => (req, res) => {
+api.patientUpdate = (models) => (req, res) => {
   // grab widget object from body
 
-  //var widget = 
-  const username = req.body.widget.username;
-  const created_at = req.body.widget.created_at;
-  const answer = req.body.widget.answer;
+  const username = req.body.username;
+  // Var treatment should be the original object that was queried, but with updated data
+  const treatment = req.body.treatment;
   
   models.DB.then(database => {
     var treatmentRepo = new models.TreatmentRepo(database);
 
     // find user in db
-    treatmentRepo.EditTreatment(username, ).then(function(value){
-      var queriedUser = value.User;
-      if (queriedUser.length === 0) {
+    treatmentRepo.EditTreatment(username, treatment);
+    res.json({success: true});
+  });
+};
+
+api.queryWidgets = (models) => (req, res) => {
+  // get username, created_at, and question array from body
+  const username = req.body.username;
+  //const password = req.body.password;
+  
+  models.DB.then(database => {
+    var treatmentRepo = new models.TreatmentRepo(database);
+    // find user in db
+    treatmentRepo.GetPatientTreatment(username).then(function(value){
+      if (!value) {
         // user was not found
         res.json({error: 'User does not exist.'});
       } else {
         // user was found
-        queriedUser = queriedUser[0];
-        // hash password from request and compare with hashed password in db
-        const passHashed = CryptoJS.HmacSHA256(password,queriedUser.identifier.salt).toString();
-        if (passHashed === queriedUser.password) {
-          if((Object.keys(queriedUser.accountType).length ===0)){
-            //send a request telling client to register the user
-            res.json({success: true, accountType: 'SSO'});
-          } else{
-            if (passHashed === queriedUser.password) {
-              res.json({success: true, accountType: queriedUser.accountType.role});       
-            } else {
-               res.json({error: 'Wrong password.'})
-            } 
-          }
-        } else {
-          res.json({error: 'Wrong password.'})
-          console.log("Wrong pass");
-
-         }
-       }
+        var treatment = value.treatments;
+        res.json({success: true, treatment: treatment});
+      }
     });
   });
 };
 
-api.patientUpdateChecklist = (models) => (req, res) => {
-    // get username, created_at, and question array from body
-    // label is "checklist"
-    //const username = req.body.username;
-    //const password = req.body.password;
-    
-    models.DB.then(database => {
-      var treatmentRepo = new models.TreatmentRepo(database);
-      // find user in db
-      treatmentRepo.FindUser(username).then(function(value){
-        var queriedUser = value.User;
-        if (queriedUser.length === 0) {
-          // user was not found
-          res.json({error: 'User does not exist.'});
-        } else {
-          // user was found
-          queriedUser = queriedUser[0];
-          // hash password from request and compare with hashed password in db
-          const passHashed = CryptoJS.HmacSHA256(password,queriedUser.identifier.salt).toString();
-          if (passHashed === queriedUser.password) {
-            if((Object.keys(queriedUser.accountType).length ===0)){
-              //send a request telling client to register the user
-              res.json({success: true, accountType: 'SSO'});
-            } else{
-              if (passHashed === queriedUser.password) {
-                res.json({success: true, accountType: queriedUser.accountType.role});       
-              } else {
-                 res.json({error: 'Wrong password.'})
-              } 
-            }
-          } else {
-            res.json({error: 'Wrong password.'})
-            console.log("Wrong pass");
+// TEMPORARY because there's no endpoint to create widget
+api.createWidget = (models) => (req, res) => {
+  // get username
+  // get widget info
+
+  // create
+  const username = req.body.username;
+  // Var treatment needs label, question, due_date
+  const treatment = req.body.treatment;
   
-           }
-         }
-      });
-    });
-  };
+  models.DB.then(database => {
+    var treatmentRepo = new models.TreatmentRepo(database);
+
+    if (treatment.label == 'meter') {
+      const treatmentObj = new models.Meter(new models.Question(treatment.question), [1, 10], treatment.due_date);
+    } else {
+      var questionArray = [];
+      for (var i=0;i<treatment.question.length;i++) {
+        questionArray.push(new models.Question(treatment.question[i]));
+      }
+      const treatmentObj = new models.Checklist(questionArray, treatment.due_date);
+    }
+    
+    // find user in db
+    treatmentRepo.CreateTreatment(username, treatmentObj);
+    res.json({success: true});
+  });
+}
 
 module.exports = api;
--- services/treatment_management/app/routes/patient_interaction.js
+++ services/treatment_management/app/routes/patient_interaction.js
@@ -5,7 +5,15 @@ module.exports = (app) => {
   const api = app.treatment_management.app.api.patient_interaction;
 
   // PUT /treatment_plan/api/patient_edit
-  // authenticates user and determines account type
-  app.route(config.routes.patientUpdate).put(api.patientUpdateMeter(models));
+  // Update patient input in treatment plan
+  app.route(config.routes.patientUpdate).put(api.patientUpdate(models));
 
+  // GET /treatment_plan/api/widgets
+  // Get all widgets for patient
+  app.route(config.routes.queryWidgets).put(api.queryWidgets(models));
+
+
+  // TEMPORARY since there's no endpoint to create a widget
+  // PUT /treatment_plan/api/create_widget
+  app.route('treatment_plan/api/create_widget').put(api.createWidget(models));
 }
