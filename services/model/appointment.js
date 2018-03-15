701ac2e8f36fbd8021c03ce2c189ccc1cdcb9240
Crystal Chun <crystalchun@crystalcomputer.lan>
2018-03-14 18:22:19 -0700

    Modified create appointment of  appointment API to check for time conflicts.

--- services/model/appointment.js
+++ services/model/appointment.js
@@ -5,6 +5,8 @@
 function Appointment(appointee, date, initiator, status) {
   // The individual that the appointment will be scheduled with
   this.appointee = appointee;
+  
+  this.appointeeName = appointeeName;
   // The date refers to the date that the appointment is requested. 
   this.date = date;
   // The start time of the appointment.
