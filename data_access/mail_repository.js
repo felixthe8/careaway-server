889414c14782c1660fbe11063ca726e9746adb2a
Taylor Tobin <19tt94@gmail.com>
2018-04-19 11:55:41 -0700

    adding mail to data access layer

---
+++ data_access/mail_repository.js
@@ -0,0 +1,100 @@
+const mongoClient = require('mongodb').MongoClient;
+const promise = require('promise');
+
+/**
+ * Constructor of the User Repository
+ * @param {*} dbConnection the database connection string
+ */
+function MailAccess(dbConnection)
+{
+  //fetches the database client connection
+  this.db = dbConnection;
+}
+
+/**
+ * This appends a new appointment into the appointment
+ * attribute of the appointee and initiator
+ * appointment list
+ *
+ * @param {*} initiator the initiator username
+ * @param {*} appointee the appointee username
+ * @param {*} message the message object that will be appended to the user
+ */
+MailAccess.prototype.CreateMail = function(initiator, appointee, message){
+  const collection = this.db.collection('Users');
+  collection.updateOne(
+    {'username' : initiator}, //looks for initiator in the database
+    { $push: {'accountType.mail': message}}, //inserts new message on the array
+    function(err, result){
+      console.log('Added message');
+    }
+  );
+  collection.updateOne(
+    {'username' : appointee},//looks for appointee username in the database
+    { $push: {'accountType.mail': message}},//inserts new message on the array
+    function(err, result){
+      console.log('Added appointment');
+   }
+  );
+};
+
+/**
+ * This gets all appointments of a particular user in the system
+ * @param {*} username the user identifier
+ * @returns {*} a promise that returns a list of appointments of a user
+ */
+MailAccess.prototype.GetMail= function(username){
+  const collection = this.db.collection('Users');
+
+  return new promise(function(fullfill,reject)
+  {
+    collection.findOne({'username' : username },function(err, result)
+    {
+      if(err)
+      {
+        console.log('Failed to get query');
+        reject(err);
+      }
+      else
+      {
+        console.log('Successfully got query');
+        if(result !== null){
+          var messages = result.accountType.mail;
+          //return an object containing all the appointments of the user
+          var messageList = {'mail' : messages};
+          fullfill(messageList);
+        }
+        else{
+          fullfill(null);
+        }
+      }
+    });
+  });
+};
+
+/**
+ * This deletes a appointment into the appointment
+ * attribute of the appointee and initiator
+ * appointment list
+ *
+ * @param {*} initiator the initiator username
+ * @param {*} appointee the appointee username
+ * @param {*} appointment the appointment object that will be deleted
+ */
+MailAccess.prototype.DeleteMail= function(initiator, appointee, message){
+  const collection = this.db.collection('Users');
+  collection.updateOne(
+    {'username' : initiator },//looks for username in the database
+    { $pull: {'accountType.mail': message}},//deletes the appointment
+      function(err, result){
+        console.log('Remove message');
+    }
+  );
+  collection.updateOne({'username' : message},//looks for username in the database
+    { $pull: {'accountType.mail': message}},//deletes the message
+      function(err, result){
+      console.log('Remove message');
+  });
+};
+
+module.exports = MailAccess;
--- services/mail_service/app/routes/mail_service.js
+++ services/mail_service/app/routes/mail_service.js
@@ -5,7 +5,7 @@ const api = require('@mailAPI/mail_service');
 module.exports = (app) => {
 
   // Call create route in config server, and post with api
-  // app.route(config.routes.createMail).post(api.createMail(models.mailRepo, models.DB));
+  app.route(config.routes.createMail).post(api.createMail(models.mailRepo, models.DB));
   // app.route(config.routes.getMail).get(api.getMail(models.mailRepo, models.DB));
   // app.route(config.routes.deleteMail).get(api.deleteMail(models.mailRepo, models.DB));
 
