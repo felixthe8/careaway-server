a9983234aea8b02cf6c07aef6aa7e493748ba068
lmao <fireclimbers@gmail.com>
2018-04-22 00:28:11 -0700

    cweated the feedback service folder-wolder with all necessawy files

---
+++ services/feedback_service/app/api/feedback.js
@@ -0,0 +1,16 @@
+const api = {};
+
+
+api.queryFeedback = (FeedbackRepo, DB) => (req, res) => {
+
+}
+
+api.createFeedback = (FeedbackRepo, DB) => (req, res) => {
+
+}
+
+api.editFeedback = (FeedbackRepo, DB) => (req, res) => {
+  
+}
+
+module.exports = api;
---
+++ services/feedback_service/app/routes/feedback.js
@@ -0,0 +1,18 @@
+const config = require('@feedbackConfig');
+const models = require('@feedbackModels');
+const api = require('@feedbackAPI/feedback');
+
+module.exports = (app) => {
+    // GET /feedback/api/feedback
+    // Retrieve all the feedbacks
+    app.route(config.routes.feedback).get(api.queryFeedback(models.FeedbackRepo, models.DB));
+    
+    // POST /feedback/api/feedback
+    // Create a feedback
+    app.route(config.routes.feedback).post(api.createFeedback(models.FeedbackRepo, models.DB));
+    
+    // PUT /feedback/api/feedback
+    // Edit a feedback
+    app.route(config.routes.feedback).put(api.editFeedback(models.FeedbackRepo, models.DB));
+
+}
